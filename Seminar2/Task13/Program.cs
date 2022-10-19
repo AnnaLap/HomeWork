1ea569e47ccc91c86327aa4a1b2ec21a6ecb8dd8
Anna Lapshina <lapshina.anna0@ya.ru>
2022-10-19 14:35:26 +0300

    add solution task 13

---
+++ Seminar2/Task13/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью 
+цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = number;
+int count = 0; 
+while (number > 0)
+{
+    number = number / 10;
+    count++;
+}
+Console.WriteLine($"Количество цифрр в числе: {count}");
+ 
+ if (count < 3)
+ {
+Console.WriteLine($"Число меньше трехзначного");
+ }
+ else
+ {
+ while (count - 3 > 0)
+ {
+    num = num / 10;
+    count--;
+ }
+Console.WriteLine($"Третья цифра числа: {num % 10}");
+ }
