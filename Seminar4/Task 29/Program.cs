8f5de1dd8836627c1173ec35c4760431bdfa4a15
Anna Lapshina <lapshina.anna0@ya.ru>
2022-11-09 15:04:56 +0300

    add solution tasks 25, 27, 29

---
+++ Seminar4/Task 29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Заполнение массива производит пользователь.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.WriteLine("Enter 8 elements of array: ");
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+   array[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Array: ");
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i != array.Length-1)
+    {
+        Console.Write(array[i]+", ");
+    }
+    else
+    Console.Write(array[i]);
+}
+Console.Write("]");
