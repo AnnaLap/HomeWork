021f31762f3f19b46cb391810aace52f24bbbea3
Anna Lapshina <lapshina.anna0@ya.ru>
2022-11-09 15:24:54 +0300

    add solution tasks 25,27,29

---
+++ Seminar4/Task 25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит
+ число A в натуральную степень B. Для решения задания использование цикла for является 
+ обязательным условием. Не использовать встроенный метод возведения в степень.
+ 3, 5 -> 243 (3⁵) 
+ 2, 4 -> 16 */
+
+//Method of exponentitation of a number
+int RaiseToPower(int num1, int num2)
+{
+    int res = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        res = res*num1;
+    } 
+    return res;
+}
+
+Console.Write("Enter A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int result = RaiseToPower(numA,numB);
+Console.Write($"{numA} to the power of {numB} is {result}");
---
+++ Seminar4/Task 27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+Для решения задания использование цикла for является обязательным условием
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+//Method of summing digits in a number
+int ToGetSum (int num)
+{
+    string StrNum = Math.Abs(num).ToString();
+    int lenght = StrNum.Length;
+    int sum = 0;
+    for (int i = 0; i < lenght; i++)
+    {
+        sum = sum + num%10;
+        num = num/10;
+    }
+    return sum;
+}
+ 
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = ToGetSum(number);
+Console.WriteLine($"The sum of digits in a number is {result}");
+
