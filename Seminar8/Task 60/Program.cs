62c433ce44d1863b9ae779dffbff10a60627480a
Anna Lapshina <lapshina.anna0@ya.ru>
2022-11-25 11:17:25 +0300

    add solution task 60

---
+++ Seminar8/Task 60/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+//To fill array with random int numbers from 10 to 99
+void FillArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                while (arr[i, j, k] == 0)
+                {
+                    int number = new Random().Next(10, 100);
+                    if (FindElement(arr, number)) continue;
+                    arr[i, j, k] = number;
+                }
+            }
+        }
+    }
+}
+
+//To check if element of array repeat 
+bool FindElement(int[,,] arr, int num)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                if (arr[i, j, k] == num) return true;
+            }
+        }
+    }
+    return false;
+}
+
+//To print element of array with indexes
+void GetIndex(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
+
+int[,,] array = new int[2, 2, 2];
+FillArray(array);
+GetIndex(array);
