583ff6057c2b0188ce4f25d6a30d4f778c597919
Anna Lapshina <lapshina.anna0@ya.ru>
2022-11-25 11:16:05 +0300

    add solution task 54

---
+++ Seminar8/Task 54/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+
+//To fill array with random int numbers from 1 to 9
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+//To show filling array
+void ShowArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+//To sort array in descending order by rows
+void DescendingSortByRows(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int k = 0; k < arr.GetLength(1) - 1; k++)
+        {
+            for (int j = 0; j < arr.GetLength(1) - 1; j++)
+            {
+                if (arr[i, j] < arr[i, j + 1])
+                {
+                    int temp = arr[i, j];
+                    arr[i, j] = arr[i, j + 1];
+                    arr[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Enter M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+Console.WriteLine("Array: ");
+ShowArray(array);
+Console.WriteLine("Array sorted in descending order by rows: ");
+DescendingSortByRows(array);
+ShowArray(array);
